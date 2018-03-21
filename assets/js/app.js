58f1959ac444399510eacbce9d556b9fb6d95b29
Travo100 <travo100@gmail.com>
2018-03-20 17:28:06 -0700

    first version of working game

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,47 +4,80 @@
 var boxes = [
     {
         name:"box box-1",
-        score: 10
+        score: 1
     },
     {
         name: "box box-2",
-        score: 12
+        score: 2
     },
     {
         name: "box box-3",
-        score: 44
+        score: 3
     }, 
     {
         name: "box box-4",
-        score: 6
+        score: 4
     }
 ];
 
-var randomNumber = 6;
+var randomNumber = getRandomInt(1, 4);
+var userGuesses = 2;
 
 $("#randomNumber").text(randomNumber);
+$("#userGuess").text(userGuesses);
 
-for (var i = 0; i < boxes.length; i++) {
-    var div = $("<div>");
-    div.addClass(boxes[i].name);
-    div.attr("data-boxscore", boxes[i].score);
-    $("#boxes").append(div);
+
+function startGame() {
+    // add the boxes 
+    // to the game with a random score 
+    // in each box
+    for (var i = 0; i < boxes.length; i++) {
+        var div = $("<div>");
+        boxes[i].score = getRandomInt(1, 4);
+        div.addClass(boxes[i].name);
+        div.attr("data-boxscore", boxes[i].score);
+        $("#boxes").append(div);
+    }
 }
+
+startGame();
+
+function restartGame() {
+    // replaces old box scores 
+    // with new box scores
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].score = getRandomInt(1, 4);
+    }
+    // update the user guesses to 2
+    userGuesses = 2;
+    // changes the randomNumber between 1 and 4
+    randomNumber = getRandomInt(1, 4);
+
+    // update the UI to reflect it
+    $("#userGuess").text(userGuesses);
+    $("#randomNumber").text(randomNumber);
+}
+
 
 // when you click on a box
 $(".box").on("click", function(){
-
+    userGuesses--;
+    $("#userGuess").text(userGuesses);
     var boxScore = parseInt($(this).attr("data-boxscore"));
     // if the score of the box 
     // matches the random number 
     // you win
     if(boxScore === randomNumber) {
-        alert("You win! They match!")
-    } else {
-         // otherwise you lose
-        alert("You lose!");
+        alert("You win! They match!");
+        restartGame();
     }
 
-   
-
+    if (userGuesses === 0) {
+        alert("You lose!");
+        restartGame();
+    }
 });
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
